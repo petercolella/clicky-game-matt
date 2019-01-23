83005621d59a4a7792da89532b3443369d32e9a6
Fazio <Matt.Fazio@stericycle.com>
2019-01-23 17:13:11 -0500

    updates

--- my-app/src/components/Footer/index.js
+++ my-app/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="bottom">
-        Clicky Game! <img alt="react" src="assets/images/react.svg" />
+        It's Clickin' GREAT!
       </div>
     </footer>
   );
